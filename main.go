3105f82244f330aa7e39ce7937a6eb9cd3a5b634
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-11 01:56:09 -0300

    :tada: cria projeto para estudos de orientação a objetos em go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+	var titular string = "Maysa"
+	var numeroAgencia int = 789
+	var numeroConta int = 654321
+	var saldo float64 =125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
