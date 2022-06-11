ea737a3232b4c6b510d2f16a6e4b6aeea799935a
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-11 03:49:54 -0300

    :sparkles: cria um package contas e altera a visibilidade das variavieis para o documento todo

--- main.go
+++ main.go
@@ -1,53 +1,16 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64){
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-
-		return "Depósito realizado com sucesso", c.saldo
-	} else {
-		return "É obrigatório que o valor depositado seja maior que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-
-	if podeSacar {
-		c.saldo -= valorDoSaque
-
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-
-		return true
-	} else {
-		return false
-	}
-
-}
+import (
+	"fmt"
+	
+	"banco/contas"
+)
 
 func main() {
-	contaMaysa := ContaCorrente { titular: "Maysa", numeroAgencia: 789, numeroConta: 654321, saldo: 150.50 }
-	contaKasper := ContaCorrente {"Kasper", 222, 111222, 200 }
-	contaIna := ContaCorrente {"Iná", 325, 3344, 300 }
-	contaNikole := ContaCorrente{"Nikole", 444, 112233, 350}
+	contaMaysa := contas.ContaCorrente { Titular: "Maysa", NumeroAgencia: 789, NumeroConta: 654321, Saldo: 150.50 }
+	contaKasper := contas.ContaCorrente {"Kasper", 222, 111222, 200 }
+	contaIna := contas.ContaCorrente {"Iná", 325, 3344, 300 }
+	contaNikole := contas.ContaCorrente{"Nikole", 444, 112233, 350}
 
 	fmt.Println(contaMaysa)
 	fmt.Println(contaKasper)
