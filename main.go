9148b46b29596c6da0bb3e93b0afc1ee5a2d9a11
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-11 02:49:18 -0300

    :sparkles: cria função de saque

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ type ContaCorrente struct {
 	saldo float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 	contaMaysa := ContaCorrente { titular: "Maysa", numeroAgencia: 789, numeroConta: 654321, saldo: 150.50 }
 
@@ -25,4 +37,13 @@ func main() {
 	contaIna.saldo = 300
 
 	fmt.Println(contaIna)
+
+	contaNikole := ContaCorrente{"Nikole", 444, 112233, 350}
+
+	fmt.Println(contaNikole.saldo)
+
+	fmt.Println(contaNikole.Sacar(200))
+
+	fmt.Println(contaNikole.saldo)
+
 }
