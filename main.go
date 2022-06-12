73947e59c02cb18d29015faed44d05347b748614
Maysa Pereira <maysalvespereira@gmail.com>
2022-06-12 02:04:13 -0300

    :sparkles: cria função de pagamento de boleto com o tipo interface

--- main.go
+++ main.go
@@ -8,13 +8,24 @@ import (
 	"banco/clientes"
 )
 
+func PagarBoleto (conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 
 	contaMaysa := contas.ContaPoupanca {}
-
 	contaMaysa.Depositar(100)
-	contaMaysa.Sacar(88)
-
+	PagarBoleto(&contaMaysa, 60)
 	fmt.Println(contaMaysa.ObterSaldo())
+
+	contaIna := contas.ContaCorrente {}
+	contaIna.Depositar(200)
+	PagarBoleto(&contaIna, 150)
+	fmt.Println(contaIna.ObterSaldo())
 	
 }
